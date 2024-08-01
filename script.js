d9e1996f51e85397bdb1b5d697b119db799eb460
Oleksii Buinyi <buinyiov@gmail.com>
2024-08-01 17:46:32 +0200

    test

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 function setLightTheme() {
-	document.body.classList.remove('dark');
-	document.body.classList.add('light');
+	document.documentElement.classList.remove('dark');
+	document.documentElement.classList.add('light');
 	localStorage.setItem('theme', 'light');
 }
 
 function setDarkTheme() {
-	document.body.classList.remove('light');
-	document.body.classList.add('dark');
+	document.documentElement.classList.remove('light');
+	document.documentElement.classList.add('dark');
 	localStorage.setItem('theme', 'dark');
 }
 
 function setSystemTheme() {
-	document.body.classList.remove('light');
-	document.body.classList.remove('dark');
+	document.documentElement.classList.remove('light');
+	document.documentElement.classList.remove('dark');
 	localStorage.removeItem('theme');
 	applySystemTheme();
 }
@@ -20,9 +20,9 @@ function setSystemTheme() {
 function applySystemTheme() {
 	const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
 	if (prefersDarkScheme) {
-		document.body.classList.add('dark');
+		document.documentElement.classList.add('dark');
 	} else {
-		document.body.classList.add('light');
+		document.documentElement.classList.add('light');
 	}
 }
 
